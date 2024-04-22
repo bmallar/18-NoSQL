97b0073ef91661618baa93ba6fecfe659f17c748
bmallar <benmallar@gmail.com>
2024-04-22 15:46:53 -0500

    set up files, add boiler plate code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 18-NoSQL
+
+
+1. [My repo Link]()
+2. [My Live URL]()
---
+++ models/thought.js
@@ -0,0 +1,7 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtSchema = new Schema
+
+
+
+module.exports = thoughtSchema;
