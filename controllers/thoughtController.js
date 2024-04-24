363377981d51d926e9d5257e122a038ec4437195
bmallar <benmallar@gmail.com>
2024-04-24 09:54:20 -0500

    Code is done need to test in insomnia

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -14,7 +14,7 @@ const thoughtController = {
 
     async getSingleThought(req, res) {
         try {
-            const dbThoughtData = await User.findOne({ _id: req.params.thoughtId})
+            const dbThoughtData = await Thought.findOne({ _id: req.params.thoughtId})
             .populate('')
 
             res.json(dbThoughtData)
@@ -22,8 +22,56 @@ const thoughtController = {
 
         }
     },
-}
 
+
+async createThought(req, res) {
+    try {
+        const dbThoughtData = await Thought.create(req.body)
+        res.json(dbThoughtData)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+        
+    }
+},
+
+async updateThought(req, res) {
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        {
+          $set: req.body,
+        },
+        {
+          runValidators: true,
+          new: true,
+        }
+      );
+
+   
+
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteThought(req, res) {
+    try {
+      const dbThoughtData = await User.findOneAndDelete({ _id: req.params.thoughtId })
+
+    
+     
+      res.json({ message: 'Thought deleted!' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+
+}
 
 module.exports = thoughtController; 
     
